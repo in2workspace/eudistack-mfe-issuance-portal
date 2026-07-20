7a25da7f5e050cbad2c320ceeb8fc39607362f98
David Segura Saumell <dsegurasmll@gmail.com>
2026-07-20 10:15:30 +0200

    chore: migrate test infrastructure from Karma to Jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  collectCoverage: true,
+  coverageDirectory: './coverage/app',
+  coverageReporters: ['lcov', 'text-summary', 'html'],
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!<rootDir>/node_modules/',
+    '!src/app/**/*.module.ts',
+  ],
+};
